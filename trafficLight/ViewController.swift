145a15028bb4cd17f2ad7f3d16b25bd5946b0992
Vasyl Larin <larinvasyl@protonmail.com>
2022-09-18 15:59:00 +0600

    Initial Commit

---
+++ trafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLight
+//
+//  Created by Ларин Василий on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
