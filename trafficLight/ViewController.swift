9f59fab2a767aa7edaf1c8edcad527304d1ac7e5
Vasyl Larin <larinvasyl@protonmail.com>
2022-09-18 18:35:35 +0600

    Added IU & switching logic

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var greenLight: UIImageView!
+    @IBOutlet weak var yellowLight: UIImageView!
+    @IBOutlet weak var redLight: UIImageView!
+    @IBOutlet weak var actionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        actionButton.layer.cornerRadius = 15
+        
+
+        greenLight.layer.cornerRadius = greenLight.bounds.width / 2.0
+
+        redLight.layer.cornerRadius = redLight.bounds.width / 2
+
+        yellowLight.layer.cornerRadius = yellowLight.bounds.width / 2
+
     }
-
-
+    
+    
+    @IBAction func actionButtonTrafficLightControl() {
+        
+        actionButton.setTitle("Next", for: .normal)
+        
+        if redLight.alpha == yellowLight.alpha {
+            redLight.alpha = CGFloat(MAXFLOAT)
+            greenLight.alpha = 0.3
+            }
+        else if yellowLight.alpha != CGFloat(MAXFLOAT) && redLight.alpha == CGFloat(MAXFLOAT) && greenLight.alpha != CGFloat(MAXFLOAT) {
+            yellowLight.alpha = CGFloat(MAXFLOAT)
+            redLight.alpha = 0.3
+        }
+        else if greenLight.alpha != CGFloat(MAXFLOAT) && yellowLight.alpha == CGFloat(MAXFLOAT) && redLight.alpha != CGFloat(MAXFLOAT) {
+            greenLight.alpha = CGFloat(MAXFLOAT)
+            yellowLight.alpha = 0.3
+        }
+    }
+    
 }
-
