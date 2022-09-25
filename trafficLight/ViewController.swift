30102d3ba2952034c98a75e5b49a91311f93dc29
Vasyl Larin <larinvasyl@protonmail.com>
2022-09-25 22:17:18 +0600

    Correct rounding, refactoring.

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,44 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-    @IBOutlet weak var greenLight: UIImageView!
-    @IBOutlet weak var yellowLight: UIImageView!
-    @IBOutlet weak var redLight: UIImageView!
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
     @IBOutlet weak var actionButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private var lightIsOn: CGFloat = 1
+    private var lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         actionButton.layer.cornerRadius = 15
         
-        
-        
-        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+    
+    override func viewWillLayoutSubviews() {
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
         yellowLight.layer.cornerRadius = yellowLight.bounds.width / 2
         redLight.layer.cornerRadius = redLight.bounds.width / 2
     }
     
     
     @IBAction func actionButtonTrafficLightControl() {
-        
         actionButton.setTitle("Next", for: .normal)
         
-        if redLight.alpha == yellowLight.alpha {
-            redLight.alpha = CGFloat(MAXFLOAT)
-            greenLight.alpha = 0.3
-        }
-        else if yellowLight.alpha != CGFloat(MAXFLOAT)
-                    && redLight.alpha == CGFloat(MAXFLOAT)
-                    && greenLight.alpha != CGFloat(MAXFLOAT) {
-            yellowLight.alpha = CGFloat(MAXFLOAT)
-            redLight.alpha = 0.3
-        }
-        else if greenLight.alpha != CGFloat(MAXFLOAT)
-                    && yellowLight.alpha == CGFloat(MAXFLOAT)
-                    && redLight.alpha != CGFloat(MAXFLOAT) {
-            greenLight.alpha = CGFloat(MAXFLOAT)
-            yellowLight.alpha = 0.3
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
         }
     }
-    
 }
+
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
+    
